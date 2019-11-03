f0df3cca69a6fc4bbe7c97ef082d597b4fbcf6a7
sachin <sachin@sachin-PC>
2019-11-03 09:54:31 +0530

    removed javascript pckage

--- src/main/java/pomDemo/DropDownInPOM.java
+++ src/main/java/pomDemo/DropDownInPOM.java
@@ -1,6 +1,6 @@
 package pomDemo;
 
-import org.openqa.selenium.JavascriptExecutor;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
